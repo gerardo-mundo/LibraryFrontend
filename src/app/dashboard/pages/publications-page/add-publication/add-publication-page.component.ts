989316a81beafee84b4c88eb2d87f1b1425df077
Gerardo Mundo <ger.mundo66@gmail.com>
2023-11-28 19:31:21 -0600

    EDIT: Modificada ruta de directorios para pages/publications-page

---
+++ src/app/dashboard/pages/publications-page/add-publication/add-publication-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-publication-page',
+  templateUrl: './add-publication-page.component.html',
+  styles: [
+  ]
+})
+export class AddPublicationPageComponent {
+
+}
---
+++ src/app/dashboard/pages/publications-page/publication-list/publications-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publications-page',
+  templateUrl: './publications-page.component.html',
+  styles: [
+  ]
+})
+export class PublicationsPageComponent {
+
+}
