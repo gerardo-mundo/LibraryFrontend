6fc8c94c31775dd5af9efd3ff76abe9b6e3b8f2e
Gerardo Mundo <ger.mundo66@gmail.com>
2024-01-03 13:11:27 -0600

    IMPL: mejoras al dashboard-add-button

--- src/app/dashboard/components/add-button/add-button.component.ts
+++ src/app/dashboard/components/add-button/add-button.component.ts
@@ -8,13 +8,14 @@ import { Component, Input } from '@angular/core';
         type="submit"
         pButton
         pRipple
+        [disabled]="invalid"
         class="sm:w-2 w-full justify-content-center"
       >
         <ng-container *ngIf="isLoading; else noLoading">
           <i class="pi pi-spin pi-spinner" style="font-size: 1.4rem"></i>
         </ng-container>
         <ng-template #noLoading>
-          <span class="mr-2 font-medium">Agregar</span>
+          <span class="mr-2 font-medium">{{label ?? 'Agregar'}}</span>
           <i class="pi pi-save"></i>
         </ng-template>
       </button>
@@ -24,4 +25,6 @@ import { Component, Input } from '@angular/core';
 })
 export class AddButtonComponent {
   @Input() isLoading!: boolean;
+  @Input() invalid!: boolean;
+  @Input() label?: string;
 }
