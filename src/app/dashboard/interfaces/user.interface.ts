030617291f22a1bb09c1cb7ed509e22063d0c994
Gerardo Mundo <ger.mundo66@gmail.com>
2024-01-02 20:09:58 -0600

    IMPL: UsersService and IUser interface

---
+++ src/app/dashboard/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    type: number;
+    name: string;
+    secondName: string | null;
+    lastName: string;
+    motherName: string;
+    enrollmentNum: string | null;
+    employeeKey: string | null;
+    address: string | null;
+}
