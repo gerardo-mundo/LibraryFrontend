38632c606b840f676d3895896047d01116dbb48c
Gerardo Mundo <ger.mundo66@gmail.com>
2024-01-03 19:47:36 -0600

    IMPL: RegisteredAdministrativesList

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -16,7 +16,7 @@ import { NewLoanPageComponent } from './pages/loans-page/new-loan-page.component
 import { DeleteLoanPageComponent } from './pages/loans-page/delete-loan-page.component';
 import { RegisteredStudentsPageComponent } from './pages/register-page/registered-students/registered-students-page.component';
 import { RegisteredProfessorsPageComponent } from './pages/register-page/registered-professors/registered-professors-page.component';
-import { RegisteredAdministrativesPageComponent } from './pages/register-page/registered-administratives-page.component';
+import { RegisteredAdministrativesPageComponent } from './pages/register-page/registered-administratives/registered-administratives-page.component';
 import { NewAccountPageComponent } from './pages/accounts-page/new-account-page.component';
 import { RegisteredAccountsPageComponent } from './pages/accounts-page/registered-accounts-page.component';
 import { ChangePasswordPageComponent } from './pages/accounts-page/change-password-page.component';
