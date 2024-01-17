c001f7503235d3ec8d1610a85bed1608d8be703c
Gerardo Mundo <ger.mundo66@gmail.com>
2024-01-16 20:09:48 -0600

    IMPL: ILoanWithBooks and IBorrowedBooks interfaces

--- src/app/dashboard/interfaces/loan.interface.ts
+++ src/app/dashboard/interfaces/loan.interface.ts
@@ -2,3 +2,17 @@ export interface ILoan {
     userId: number;
     borrowedBooks: number[];
 }
+export interface ILoanWithBooks {
+    id:             number;
+    returned:       boolean;
+    loanDate:       string;
+    devolutionDate: string;
+    accountId:      string;
+    userId:         number;
+    borrowedBooks:  IBorrowedBook[];
+}
+
+export interface IBorrowedBook {
+    id:          number;
+    adquisition: number;
+}
