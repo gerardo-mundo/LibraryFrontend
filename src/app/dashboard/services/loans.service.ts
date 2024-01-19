47458fe3fe67fb922a43cde0b93325085b0d2e2d
Gerardo Mundo <ger.mundo66@gmail.com>
2024-01-18 20:13:25 -0600

    IMPL: deleteLoan and updateLoan services

--- src/app/dashboard/services/loans.service.ts
+++ src/app/dashboard/services/loans.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
-import { Observable, catchError } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 
 import { ILoan, ILoanWithBooks } from '../interfaces/loan.interface';
 import { ENVIRONMENT } from 'src/app/environments/environment';
@@ -28,4 +28,32 @@ export class LoansService {
     )
   };
 
+  public updateLoan(id: number): Observable<HttpResponse<null>> {
+    if(!id) throwError(() => new Error('Se necesita un ID'));
+
+    const date = new Date();
+
+    const data = [
+      {
+        path: "/DevolutionDate",
+        op: "replace",
+        value: date.toString()
+      },
+      {
+        path: "/Returned",
+        op: "replace",
+        value: true
+      }
+    ];
+
+    return this.http.patch<HttpResponse<null>>(`${this.BASE_URL}/loans/${id}`, data).pipe(
+      catchError((response: HttpErrorResponse) => handleErrors(response))
+    );
+  };
+
+  public deleteLoan(id: number): Observable<HttpResponse<null>> {
+    return this.http.delete<HttpResponse<null>>(`${this.BASE_URL}/loans/${id}`).pipe(
+      catchError((response: HttpErrorResponse) => handleErrors(response))
+    );
+  };
 }
