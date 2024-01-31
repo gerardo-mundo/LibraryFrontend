a04fb24eeca9341050c87e3251fbd7418e305c0b
Gerardo Mundo <ger.mundo66@gmail.com>
2024-01-30 19:28:56 -0600

    IMPL: Improves to add-button.component

--- src/app/dashboard/components/add-button/add-button.component.ts
+++ src/app/dashboard/components/add-button/add-button.component.ts
@@ -8,15 +8,11 @@ import { Component, Input } from '@angular/core';
         pButton
         pRipple
         [disabled]="invalid"
-        class="sm:w-auto w-full justify-content-center mt-4"
+        [loading]="isLoading"
+        class="sm:w-max w-full justify-content-center mt-4"
       >
-        <ng-container *ngIf="isLoading; else noLoading">
-          <i class="pi pi-spin pi-spinner" style="font-size: 1.4rem"></i>
-        </ng-container>
-        <ng-template #noLoading>
-          <span class="mr-2 font-medium">{{label ?? 'Agregar'}}</span>
-          <i class="pi pi-save"></i>
-        </ng-template>
+        <span class="mr-2 font-medium">{{label ?? 'Agregar'}}</span>
+        <i class="pi pi-save"></i>
       </button>
   `,
   styles: [],
