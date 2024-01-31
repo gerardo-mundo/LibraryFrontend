90a7cfc73d1ad42090566b502581f9b0446c4708
Gerardo Mundo <ger.mundo66@gmail.com>
2024-01-30 18:58:45 -0600

    CORR: implements sessionStorage on auth.interceptor.ts

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -5,7 +5,7 @@ import { IAuthenticationResponse } from '../auth/interfaces/login.interface';
 
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const parsedToken: IAuthenticationResponse = JSON.parse(localStorage.getItem('token') || 'null');
+    const parsedToken: IAuthenticationResponse = JSON.parse(sessionStorage.getItem('token') || 'null');
     const token = parsedToken?.token;
 
     if(parsedToken) {
