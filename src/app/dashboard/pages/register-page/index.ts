0600a3916b32b6bb6a5ed90cf383f066187bc77c
Gerardo Mundo <ger.mundo66@gmail.com>
2024-02-02 15:05:36 -0600

    IMPL: barrel file on register-page

---
+++ src/app/dashboard/pages/register-page/index.ts
@@ -0,0 +1,4 @@
+export * from './register-page/register-page.component';
+export * from './registered-administratives/registered-administratives-page.component';
+export * from './registered-professors/registered-professors-page.component';
+export * from './registered-students/registered-students-page.component';
