3b0c8e20d2bf9354c263ac9b35950b03c00d8c38
Gerardo Mundo <ger.mundo66@gmail.com>
2024-02-06 19:44:29 -0600

    IMPL: Changes to login-page.component; added encryptation functions; added p-password component

---
+++ src/app/auth/utils/encrypt.ts
@@ -0,0 +1,14 @@
+import * as cryptoJS from 'crypto-js';
+import { ENVIRONMENT } from 'src/app/environments/environment';
+
+
+export const encrypt = (value: string): string => 
+    cryptoJS.AES.encrypt(value, ENVIRONMENT.ENCRYPTATION_KEY).toString();
+
+export const decrypt = (value: string): string | null => {
+    const val = cryptoJS.AES.decrypt(value, ENVIRONMENT.ENCRYPTATION_KEY).toString(cryptoJS.enc.Utf8);
+    
+    if(!val) return null;
+
+    return val;
+};
