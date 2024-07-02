e52b25b520327b840f6d6188d83d24070cfd1c40
Gerardo Mundo <ger.mundo66@gmail.com>
2024-07-02 16:50:32 -0600

    To uppercase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
-export const environment = {
+export const ENVIRONMENT = {
 	production: false,
 	BASE_URL: 'http://localhost:8080/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const environment = {
+export const ENVIRONMENT = {
 	production: true,
 	BASE_URL: 'http://localhost:8080/api',
 };
