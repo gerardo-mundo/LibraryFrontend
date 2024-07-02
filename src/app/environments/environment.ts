fe1c06030cfe2b702c620114e9c0a6ae15a94c68
Gerardo Mundo <ger.mundo66@gmail.com>
2024-07-01 20:44:19 -0600

    Add enviroments vars

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const ENVIRONMENT = {
-	BASE_URL: 'http://localhost:8080/api',
-	ENCRYPTATION_KEY: 'urhjdnbsurghdncbbsurhbjksurgrksb',
+	BASE_URL: process.env['API_URL'],
+	ENCRYPTATION_KEY: process.env['ENCRYPT_KEY'],
 };
