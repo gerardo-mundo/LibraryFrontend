bd971dec7225c2caabbda2c3607cda064d16bdb5
Gerardo Mundo <ger.mundo66@gmail.com>
2024-07-03 18:29:14 -0600

    Fix development base url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const ENVIRONMENT = {
 	production: false,
-	BASE_URL: import.meta.env['APP_ENCRYPTATION_KEY'],
+	BASE_URL: import.meta.env['APP_BASE_URL'],
 	ENCRYPTED_KEY: import.meta.env['APP_ENCRYPTATION_KEY'],
 };
