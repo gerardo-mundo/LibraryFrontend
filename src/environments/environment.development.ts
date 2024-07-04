7e513b82304594755c1928391e8f59bc9748ef4e
Gerardo Mundo <ger.mundo66@gmail.com>
2024-07-03 18:06:12 -0600

    Add correct import

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const ENVIRONMENT = {
 	production: false,
-	BASE_URL: 'http://localhost:8080/api',
+	BASE_URL: import.meta.env['APP_ENCRYPTATION_KEY'],
 	ENCRYPTED_KEY: import.meta.env['APP_ENCRYPTATION_KEY'],
 };
