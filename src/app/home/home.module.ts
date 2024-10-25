481bff80f8a43fc8ea5371a58ee4f2dfa7598c1e
Gerardo Mundo <ger.mundo66@gmail.com>
2024-10-24 18:09:17 -0600

    Relocate home-page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { HomePageComponent } from './home-page/home-page.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
 	declarations: [HomePageComponent],
