dd6998c0a2b2d04cd737a63663d4bada95b90a04
Gerardo Mundo <ger.mundo66@gmail.com>
2024-10-25 19:46:12 -0600

    Add Home initial template

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+// eslint-disable-next-line import/order
+import { PrimeNgModule } from '../primeNg/prime-ng.module';
 
 @NgModule({
 	declarations: [HomePageComponent],
-	imports: [CommonModule, HomeRoutingModule],
+	imports: [CommonModule, HomeRoutingModule, PrimeNgModule],
 })
 export class HomeModule {}
